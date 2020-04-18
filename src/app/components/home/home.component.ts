019c57b972bfaaf810fb34289efd97ff54393367
sachikantatulu <sachikantajena74@gmail.com>
2020-04-18 14:15:49 +0530

    initial commit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { BikeService} from '../../services/bike.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  models: string[] =[
+     'Honda Activa H2002G The Smart Series',
+     'Suzuki Access 125 road racer',
+     'Vezpa 150 The Stylish Bike'
+   ];
+   bikeForm: FormGroup;
+   validMessage: string = "";
+
+  constructor(private bikeService:BikeService) { }
+
+  ngOnInit(){
+   
+  this.bikeForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    phone: new FormControl('', Validators.required),
+    model: new FormControl('', Validators.required),
+    serialNumber: new FormControl('', Validators.required),
+    purchasePrice: new FormControl('', Validators.required),
+    purchaseDate: new FormControl('', Validators.required),
+    contact: new FormControl()
+
+});
+  }
+submitRegistration() {
+   if(this.bikeForm.valid) { 
+    this.validMessage ="Your Bike Registration has been submited. Thank You!";
+    this.bikeService.createBikeRegistration(this.bikeForm.value).subscribe(
+      data => {
+       this.bikeForm.reset();
+       return true;
+      },
+      error => {
+        return Observable.throw(error);
+      }
+     )
+    } else {
+      this.validMessage = "Please fill out the form before submitting!";
+    }
+   }
+}
